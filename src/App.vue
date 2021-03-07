8c77bcc0cf0e7e7ef043e5cba81592d177aa4d01
pedronogs <phnogs@hotmail.com>
2021-03-06 23:30:11 -0300

    [Style and Vuetify] Added Vuetify to project / First template for home page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,75 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <v-app>
+    <div class="d-flex flex-column h-100">
+      <!-- Navigation bar -->
+      <div class="d-flex align-center justify-space-around" id="navbar">
+        <div class="flex-same-width white--text font-weight-black">
+          (logo) Projeto Recomeçar
+        </div>
+        <div class="flex-same-width">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </div>
+        <div class="flex-same-width white--text">
+          Links
+        </div>
+      </div>
+
+      <!-- Route visualization -->
+      <div id="background">
+        <div id="main-container" class="d-flex align-center justify-center">
+          <router-view />
+        </div>
+      </div>
     </div>
-    <router-view />
-  </div>
+  </v-app>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+// SASS variables
+$primary-color: #1cce1cd7;
+
+// Global configurations
+p {
+  text-indent: 15px;
 }
 
-#nav {
-  padding: 30px;
+// General Classes
+.h-100 {
+  height: 100% !important;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.flex-same-width {
+  display: flex;
+  justify-content: center;
+  flex: 1 1 0px;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.primary-color {
+  color: $primary-color;
+}
+
+// Specific ID's
+#navbar {
+  height: 5%;
+  background-color: #000000;
+}
+
+#background {
+  height: 95%;
+  width: 100%;
+  // background-color: #1c1c1c;
+  background-image: url("./assets/background.jpeg") !important;
+  background-size: cover;
+}
+
+#main-container {
+  padding-left: 10vw;
+  padding-right: 10vw;
+
+  background-color: rgba(0, 0, 0, 0.7);
+
+  height: 100%;
+  width: 100%;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div id="home-container" class="d-flex flex-column flex-md-row">
+    <div class="flex-same-width flex-column">
+      <h1 class="text-h1 font-weight-bold primary-color">Olá !</h1>
+
+      <p class="text-subtitle-1 white--text text-justify">
+        Somos as psicólogas Ruth Braga (CRP 01-20774), Natália Ribeiro (CRP
+        01-23593) e Nicole Xavier (CRP-01/23718), estamos disponibilizando vagas
+        para atendimento social comunitário online.
+      </p>
+
+      <p class="text-subtitle-1 white--text text-justify">
+        Decidimos criar este projeto com o intuito de proporcionar à comunidade
+        os conhecimentos que nos foram adquiridos em nossos anos de formação e
+        aprimoramento.
+      </p>
+    </div>
+
+    <div class="flex-same-width">Teste</div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default Vue.extend({
   name: "Home",
-  components: {
-    HelloWorld
-  }
 });
 </script>
+
+<style lang="scss">
+#home-container {
+  padding: 50px;
+  background-color: black;
+}
+</style>
