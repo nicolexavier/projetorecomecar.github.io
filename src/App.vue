8960ac5a2394c48a8e7f905bd841613bb4b8f5e6
pedronogs <phnogs@hotmail.com>
2021-03-10 01:10:10 -0300

    [Refactor Style] Changed entire page flexbox styles for complete responsivity

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <v-app>
-    <div class="d-flex flex-column">
-      <!-- Navigation bar -->
-      <div class="d-flex align-center justify-space-around" id="navbar">
-        <div class="white--text font-weight-black">
-          Projeto Recomeçar
-        </div>
-
-        <v-btn
-          style="color: black; background-color: white; font-weight: bold;"
-          href="https://docs.google.com/forms/d/1KVUyfst4wL4fvyX1bqX_lesBq6Sqa1OtkSjs4hIG-VE/edit?usp=sharing"
-          target="_blank"
-          >Inscrição</v-btn
-        >
+    <!-- Navigation bar -->
+    <div class="d-flex align-center justify-space-around" id="navbar">
+      <div class="white--text font-weight-black">
+        Projeto Recomeçar
       </div>
 
-      <!-- Route visualization -->
-      <router-view />
+      <v-btn
+        style="color: white; font-weight: bold; max-height: 3vh;"
+        color="primary"
+        href="https://docs.google.com/forms/d/1KVUyfst4wL4fvyX1bqX_lesBq6Sqa1OtkSjs4hIG-VE/edit?usp=sharing"
+        target="_blank"
+        >Inscrição</v-btn
+      >
     </div>
+
+    <!-- Route visualization -->
+    <router-view />
   </v-app>
 </template>
 
@@ -28,13 +27,17 @@ $secondary-color: #93a889;
 
 // Global configurations
 html {
+  min-height: 100%;
+  overflow-y: scroll;
   overflow-x: none;
 }
 
 body {
   width: 100%;
   height: 100%;
-  margin: 0;
+  min-height: 100%;
+  margin: 0px;
+  padding: 0px;
 }
 
 p {
@@ -67,11 +70,25 @@ p {
   color: $secondary-color;
 }
 
+.main-text {
+  font-size: 1rem;
+}
+
 // Specific ID's
 #navbar {
-  position: fixed;
   height: 5vh;
-  width: 100vw;
   background-color: #3e563e;
+}
+
+@media (max-width: 960px) {
+  .main-text {
+    font-size: 1rem;
+  }
+}
+
+@media (min-width: 961px) {
+  .main-text {
+    font-size: 1.2rem;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,55 +1,47 @@
 <template>
-  <div class="d-flex flex-column">
-    <div id="about-background">
+  <div style="position: relative; height: 100%;">
+    <div style="height: 88vh;" id="about-background">
       <div
-        class="d-flex align-md-center justify-center mt-10 mt-md-0 h-100"
-        style="background-color: rgba(0,0, 0, 0.75); background-attachment: scroll !important;"
+        class="d-flex flex-column flex-md-row justify-center align-center container-spacing"
+        style="height: 100%; background-color: rgba(0,0, 0, 0.75); background-attachment: scroll !important;"
       >
-        <div
-          class="d-flex flex-column flex-md-row align-center container-spacing"
-        >
-          <div class="d-flex flex-column pa-10 pr-md-16">
-            <h1 class="text-h1 font-weight-bold secondary-color">Olá!</h1>
+        <div class="d-flex flex-column px-11 py-4 pr-md-16 pa-md-10">
+          <h1 class="text-h1 font-weight-bold secondary-color">Olá!</h1>
 
-            <p class="text-subtitle-1 white--text text-justify">
-              Somos as psicólogas <strong>Natália Ribeiro</strong> (CRP
-              01-23593), <strong>Nicole Xavier</strong> (CRP 01-23718) e
-              <strong>Ruth Braga</strong> (CRP 01-20774), estamos
-              disponibilizando vagas para atendimento social online.
-            </p>
+          <p class="main-text white--text text-justify">
+            Somos as psicólogas <strong>Natália Ribeiro</strong> (CRP 01-23593),
+            <strong>Nicole Xavier</strong> (CRP 01-23718) e
+            <strong>Ruth Braga</strong> (CRP 01-20774), estamos disponibilizando
+            vagas para atendimento social online.
+          </p>
 
-            <p class="text-subtitle-1 white--text text-justify">
-              Decidimos criar este projeto com o intuito de proporcionar à
-              comunidade os conhecimentos que nos foram adquiridos em nossos
-              anos de formação e aprimoramento.
-            </p>
-          </div>
-
-          <img src="../assets/psicologo.jpg" />
+          <p class="main-text white--text text-justify">
+            Decidimos criar este projeto com o intuito de proporcionar à
+            comunidade os conhecimentos que nos foram adquiridos em nossos anos
+            de formação e aprimoramento.
+          </p>
         </div>
+
+        <img src="../assets/psicologo.jpg" />
       </div>
     </div>
 
     <div
-      class="d-flex flex-column flex-md-row align-center justify-center container-spacing"
-      style="height: 60vh;"
-      id="second-section"
+      class="d-flex flex-column flex-md-row align-center justify-center container-spacing pt-10 pb-md-10"
     >
       <h1
         class="text-h2 text-md-h1 text-center font-weight-bold secondary-color"
       >
-        "Como a psicoterapia pode me ajudar?"
+        “Como a psicoterapia pode me ajudar?”
       </h1>
 
-      <div class="d-flex flex-column pa-10">
-        <p
-          class="text-subtitle-1 black--text font-weight-bold text-justify mt-3"
-        >
+      <div class="d-flex flex-column py-5 px-11">
+        <p class="main-text black--text font-weight-bold text-justify mt-3">
           A psicoterapia é um processo seu, que exigirá um trabalho colaborativo
           entre o psicólogo e você.
         </p>
 
-        <p class="text-subtitle-1 black--text font-weight-bold text-justify">
+        <p class="main-text black--text font-weight-bold text-justify">
           O psicólogo irá te auxiliar na busca por caminhos que façam sentido
           para você, com intuito de alcançar seus objetivos, utilizando de
           escuta sem julgamentos, empatia e acolhimento.
@@ -58,37 +50,37 @@
     </div>
 
     <div
-      class="d-flex flex-column flex-md-row align-center justify-center container-spacing"
-      style="height: 60vh; background-color: #93a889;"
-      id="third-section"
+      class="d-flex flex-column flex-md-row align-center justify-center container-spacing pt-10 pb-md-16"
+      style="background-color: #93a889;"
     >
       <img src="../assets/atendimento.jpg" />
 
-      <div class="d-flex flex-column pa-10">
+      <div class="d-flex flex-column py-6 px-11 ml-md-10">
         <h1 class="text-h2 text-md-h1 font-weight-bold white--text">
           Participe!
         </h1>
 
-        <p class="text-subtitle-1 white--text text-justify mt-3">
+        <p class="main-text white--text text-justify mt-3">
           Este projeto busca promover acessibilidade ao atendimento psicológico
           para a população. Este é um projeto autônomo, sem parceria com
           instituições, feito por psicólogas.
         </p>
 
-        <p class="text-subtitle-1 white--text text-justify">
+        <p class="main-text white--text text-justify">
           As sessões de atendimento serão exclusivamente online, com duração
           entre 45 e 50 minutos, semanalmente, e serão disponibilizadas de
           acordo com a disponibilidade de agenda das psicólogas.
         </p>
 
-        <p class="text-subtitle-1 white--text text-justify">
+        <p class="main-text white--text text-justify">
           Para mais informações, se inscreva pelo formulário clicando no link
           abaixo:
         </p>
 
         <v-btn
-          class="mt-5 mb-8 mb-md-5"
+          class="mt-5 mb-14 mb-md-5"
           id="subscribe-btn"
+          color="primary"
           href="https://docs.google.com/forms/d/1KVUyfst4wL4fvyX1bqX_lesBq6Sqa1OtkSjs4hIG-VE/edit?usp=sharing"
           target="_blank"
           >Increva-se</v-btn
@@ -96,10 +88,7 @@
       </div>
     </div>
 
-    <div
-      id="footer"
-      class="d-flex justify-center align-center text-md-body-2 grey--text"
-    >
+    <div id="footer">
       © 2021 - Direitos reservados às sócias do Projeto Recomeçar.
     </div>
   </div>
@@ -118,21 +107,22 @@ export default Vue.extend({
   background-image: url("../assets/monstera.png") !important;
   background-size: cover;
   background-attachment: fixed;
-  background-repeat: no-repeat;
-}
-
-#footer {
-  position: relative;
-  height: 5vh;
-  width: 100vw;
-
-  background-color: black;
 }
 
 #subscribe-btn {
   color: white;
   font-weight: bolder;
-  background-color: #40e0d0;
+}
+
+#footer {
+  color: grey;
+  font-size: 0.8rem;
+  padding: 10px;
+  background-color: black;
+  text-align: center;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
 }
 
 @media (max-width: 960px) {
@@ -147,23 +137,6 @@ export default Vue.extend({
     padding-left: 0;
     padding-right: 0;
   }
-
-  #second-section {
-    margin-top: 15vh;
-  }
-
-  #third-section {
-    margin-top: 10vh;
-
-    img {
-      margin-top: 60vh;
-    }
-  }
-
-  #footer {
-    margin-top: 20vh;
-    font-size: 10px;
-  }
 }
 
 @media (min-width: 960px) {
@@ -175,8 +148,8 @@ export default Vue.extend({
   }
 
   .container-spacing {
-    padding-left: 15vw;
-    padding-right: 15vw;
+    padding-left: 12vw;
+    padding-right: 12vw;
   }
 }
 </style>
